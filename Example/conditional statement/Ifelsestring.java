35b0b9ffbfa114c77aab1b59ed671ea15415117f
Anitha-sai26 <anitha.chinnasamy@archimedis.io>
2025-03-14 17:38:33 +0530

    ifelse string added

---
+++ Example/conditional statement/Ifelsestring.java
@@ -0,0 +1,18 @@
+public class Ifelsestring {
+    public static void main(String args[])
+    {
+        String color="orange";
+        if(color.equals("green"))
+        {
+            System.out.println("online");
+        }
+        else if(color.equals("orange"))
+        {
+            System.out.println("away");
+        }
+        else
+        {
+            System.out.println("offline");
+        }
+    }
+}
